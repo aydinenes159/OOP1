69674fceebb73d7a1f076cb6c2e7913e7ccec5fc
aydinenes159@gmail.com <aydinenes159@gmail.com>
2021-01-20 23:51:56 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int catagoryId { get; set; }
+        
+        public string productName { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int UnitsIdStock { get; set; }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.catagoryId = 2;
+            product1.productName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsIdStock = 3;
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine();
+    }
+    }
+}
